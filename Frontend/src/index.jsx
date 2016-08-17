241a4546c0e0cb2827295d3bfa32b8f56dade1f0
Yash Shah <mail@yashshah.com>
2016-08-18 01:40:31 +0530

    Add the routes in index.jsx

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -3,14 +3,21 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import { render } from 'react-dom';
 import {ServiceDisruptionPage} from './components/ServiceDisruptionPage'
+import {ManageServiceDisruption} from './components/ManageServiceDisruption'
+import {ServiceDisruptionList} from './components/ServiceDisruptionList'
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 class App extends React.Component {
   render() {
     return(
-      <div>
-        <ServiceDisruptionPage / >
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={ServiceDisruptionPage}>
+          <IndexRoute component={ServiceDisruptionList} />
+          <Route path="add" component={ManageServiceDisruption}>
+          </Route>
+        </Route>
+      </Router>
     )
   }
 }
