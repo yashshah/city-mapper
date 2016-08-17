f8dfed9e8dccc14cc42ea244224a657d9350aa73
Yash Shah <mail@yashshah.com>
2016-08-17 22:57:18 +0530

    Create AppBar component

---
+++ Frontend/src/components/AppBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Appbar from 'material-ui/AppBar';
+
+const AppBar = () => (
+  <Appbar
+    title="Service Disruption Management"
+  />
+);
+
+export default AppBar;
