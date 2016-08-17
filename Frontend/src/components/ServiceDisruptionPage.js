a10705136b481d294e4479f1c45a67ef29431175
Yash Shah <mail@yashshah.com>
2016-08-17 23:15:55 +0530

    Displaying all the components in DisruptionPage component

--- Frontend/src/components/ServiceDisruptionPage.js
+++ Frontend/src/components/ServiceDisruptionPage.js
@@ -1,19 +1,28 @@
 import { default as React, Component } from 'react';
 import { render } from 'react-dom';
 import {ServiceDisruptionList} from './ServiceDisruptionList'
+import {ServiceDisruptionForm} from './ServiceDisruptionForm'
+import AppBar from './AppBar'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 export class ServiceDisruptionPage extends Component {
   constructor(props) {
     super(props);
-  
     this.state = {};
   }
 
   render() {
     return(
       <div>
-        <h3> Service Disruption Page </h3>
-        <ServiceDisruptionList />
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <div>
+            <AppBar />
+            <div className="container">
+              <ServiceDisruptionList />
+            </div>
+          </div>
+        </MuiThemeProvider>
       </div>
     )
   }
