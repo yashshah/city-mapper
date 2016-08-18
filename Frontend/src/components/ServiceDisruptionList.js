8bebffdfc226b7142768ad286c6b1664eebe819f
Yash Shah <mail@yashshah.com>
2016-08-18 21:32:49 +0530

    Using data directly from database.js

--- Frontend/src/components/ServiceDisruptionList.js
+++ Frontend/src/components/ServiceDisruptionList.js
@@ -4,6 +4,7 @@ import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import {CardItem} from './CardItem';
+import serviceDisruptionData from './database.js'
 
 const styles = {
   root: {
@@ -16,51 +17,51 @@ const styles = {
   },
 };
 
-const serviceDisruptionData = [
-  {
-    title: 'Toronto - Paris',
-    subtitle: '...Bond. James Bond.',
-  },
-  {
-    title: 'New York - Boston',
-    subtitle: 'Of all the gin joints in all the towns in all the world, she walks into mine.',
-  },
-  {
-    title: 'San Fransico - Seattle',
-    subtitle: 'Well, its not the men in your life that counts, its the life in your men.',
-  },
-  {
-    title: 'Bangkok - Hanoi',
-    subtitle: 'Would you be shocked if I put on something more comfortable?',
-  },
-  {
-    title: 'Delhi - Bangalore',
-    subtitle: 'My Mama always said, Life was like a box of chocolates; you never know what you are gonna get.',
-  },
-  {
-    title: 'Rome - Venice',
-    subtitle: 'I could dance with you till the cows come home...On second thought, I had rather dance with the cows when',
-  },
-  {
-    title: 'Munich - Frankfurt',
-    subtitle: 'Frankly, my dear, I dont give a damn!',
-  },
-  {
-    title: 'Seattle - Portland',
-    subtitle: 'You talking to me? You talking to me? You talking to me? Well, who the hell else are you talking to?',
-  },
-  {
-    title: 'Barcelona - Madrid',
-    subtitle: 'Gimme a whiskey, ginger ale on the side. And dont be stingy, baby.',
-  },
-];
+// const serviceDisruptionData = [
+//   {
+//     title: 'Toronto - Paris',
+//     subtitle: '...Bond. James Bond.',
+//   },
+//   {
+//     title: 'New York - Boston',
+//     subtitle: 'Of all the gin joints in all the towns in all the world, she walks into mine.',
+//   },
+//   {
+//     title: 'San Fransico - Seattle',
+//     subtitle: 'Well, its not the men in your life that counts, its the life in your men.',
+//   },
+//   {
+//     title: 'Bangkok - Hanoi',
+//     subtitle: 'Would you be shocked if I put on something more comfortable?',
+//   },
+//   {
+//     title: 'Delhi - Bangalore',
+//     subtitle: 'My Mama always said, Life was like a box of chocolates; you never know what you are gonna get.',
+//   },
+//   {
+//     title: 'Rome - Venice',
+//     subtitle: 'I could dance with you till the cows come home...On second thought, I had rather dance with the cows when',
+//   },
+//   {
+//     title: 'Munich - Frankfurt',
+//     subtitle: 'Frankly, my dear, I dont give a damn!',
+//   },
+//   {
+//     title: 'Seattle - Portland',
+//     subtitle: 'You talking to me? You talking to me? You talking to me? Well, who the hell else are you talking to?',
+//   },
+//   {
+//     title: 'Barcelona - Madrid',
+//     subtitle: 'Gimme a whiskey, ginger ale on the side. And dont be stingy, baby.',
+//   },
+// ];
 
 export class ServiceDisruptionList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       deleteDialogOpen: false,
-      items: serviceDisruptionData
+      items: serviceDisruptionData.serviceDisruption
     }
     this.handleDelete = this.handleDelete.bind(this);
   }
@@ -69,7 +70,7 @@ export class ServiceDisruptionList extends Component {
   handleDelete(data){
     let updatedItems = this.state.items;
     updatedItems = updatedItems.filter(function( obj ) {
-        return obj.title !== data.title;
+        return obj.id !== data.id;
     });
     this.setState({
       deleteDialogOpen: false,
@@ -82,7 +83,7 @@ export class ServiceDisruptionList extends Component {
     return(
       <div className="row">
         {this.state.items.map((data) => (
-          <div className="col s12 m12" key={data.title} style={styles.gridList}>
+          <div className="col s12 m12" key={data.id} style={styles.gridList}>
             <CardItem data={data} onDelete={this.handleDelete} />
           </div>
         ))}
