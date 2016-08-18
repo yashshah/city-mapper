ba8fcfc8ad8bbde4836e2dcef42279ee97bc1cd1
Yash Shah <mail@yashshah.com>
2016-08-18 20:32:22 +0530

    Fix the data format

---
+++ Frontend/src/components/data.js
@@ -0,0 +1,337 @@
+module.exports = {
+  "routes": [
+    {
+      "id": 1,
+      "brand_id": "Subway",
+      "short_name": "1",
+      "name": "Yonge-University",
+      "branches": [
+            {
+              "id": 1,
+              "name": "Downsview"
+            },
+            {
+              "id": 2,
+              "name": "Wilson"
+            },
+            {
+              "id": 3,
+              "name": "Yorkdale"
+            },
+            {
+              "id": 4,
+              "name": "Lawrence West"
+            },
+            {
+              "id": 5,
+              "name": "Glencairn"
+            },
+            {
+              "id": 6,
+              "name": "Eglinton West"
+            },
+            {
+              "id": 7,
+              "name": "St Clair West"
+            },
+            {
+              "id": 8,
+              "name": "Dupont"
+            },
+            {
+              "id": 9,
+              "name": "Spadina"
+            },
+            {
+              "id": 10,
+              "name": "St George"
+            },
+            {
+              "id": 11,
+              "name": "Museum"
+            },
+            {
+              "id": 12,
+              "name": "Queen's Park"
+            },
+            {
+              "id": 13,
+              "name": "St Patrick"
+            },
+            {
+              "id": 14,
+              "name": "Osgoode"
+            },
+            {
+              "id": 15,
+              "name": "St Andrew"
+            },
+            {
+              "id": 16,
+              "name": "Union"
+            },
+            {
+              "id": 17,
+              "name": "King"
+            },
+            {
+              "id": 18,
+              "name": "Queen"
+            },
+            {
+              "id": 19,
+              "name": "Dundas"
+            },
+            {
+              "id": 20,
+              "name": "College"
+            },
+            {
+              "id": 21,
+              "name": "Wellesley"
+            },
+            {
+              "id": 22,
+              "name": "Bloor-Yonge"
+            },
+            {
+              "id": 23,
+              "name": "Rosedale"
+            },
+            {
+              "id": 24,
+              "name": "Summerhill"
+            },
+            {
+              "id": 25,
+              "name": "St Clair"
+            },
+            {
+              "id": 26,
+              "name": "Davisville"
+            },
+            {
+              "id": 27,
+              "name": "Eglinton"
+            },
+            {
+              "id": 28,
+              "name": "Lawrence"
+            },
+            {
+              "id": 29,
+              "name": "York Mills"
+            },
+            {
+              "id": 30,
+              "name": "Sheppard-Yonge"
+            },
+            {
+              "id": 31,
+              "name": "North York Centre"
+            },
+            {
+              "id": 32,
+              "name": "Finch"
+            }
+      
+      ]
+    },
+    {
+      "id": 2,
+      "brand_id": "Subway",
+      "short_name": "2",
+      "name": "Bloor-Danforth",
+      "branches": [
+       
+            {
+              "id": 33,
+              "name": "Kipling"
+            },
+            {
+              "id": 34,
+              "name": "Islington"
+            },
+            {
+              "id": 35,
+              "name": "Royal York"
+            },
+            {
+              "id": 36,
+              "name": "Old Mill"
+            },
+            {
+              "id": 37,
+              "name": "Jane"
+            },
+            {
+              "id": 38,
+              "name": "Runnymede"
+            },
+            {
+              "id": 39,
+              "name": "High Park"
+            },
+            {
+              "id": 40,
+              "name": "Keele"
+            },
+            {
+              "id": 41,
+              "name": "Dundas West"
+            },
+            {
+              "id": 42,
+              "name": "Lansdowne"
+            },
+            {
+              "id": 43,
+              "name": "Dufferin"
+            },
+            {
+              "id": 44,
+              "name": "Ossington"
+            },
+            {
+              "id": 45,
+              "name": "Christie"
+            },
+            {
+              "id": 46,
+              "name": "Bathurst"
+            },
+            {
+              "id": 9,
+              "name": "Spadina"
+            },
+            {
+              "id": 10,
+              "name": "St George"
+            },
+            {
+              "id": 47,
+              "name": "Bay"
+            },
+            {
+              "id": 22,
+              "name": "Bloor-Yonge"
+            },
+            {
+              "id": 48,
+              "name": "Sherbourne"
+            },
+            {
+              "id": 49,
+              "name": "Castle Frank"
+            },
+            {
+              "id": 50,
+              "name": "Broadview"
+            },
+            {
+              "id": 51,
+              "name": "Chester"
+            },
+            {
+              "id": 52,
+              "name": "Pape"
+            },
+            {
+              "id": 53,
+              "name": "Donlands"
+            },
+            {
+              "id": 54,
+              "name": "Greenwood"
+            },
+            {
+              "id": 55,
+              "name": "Coxwell"
+            },
+            {
+              "id": 56,
+              "name": "Woodbine"
+            },
+            {
+              "id": 57,
+              "name": "Main Street"
+            },
+            {
+              "id": 58,
+              "name": "Victoria Park"
+            },
+            {
+              "id": 59,
+              "name": "Warden"
+            },
+            {
+              "id": 60,
+              "name": "Kennedy"
+            }
+      
+      ]
+    },
+    {
+      "id": 3,
+      "brand_id": "Subway",
+      "short_name": "3",
+      "name": "Scarborough",
+      "branches": [
+            {
+              "id": 60,
+              "name": "Kennedy"
+            },
+            {
+              "id": 61,
+              "name": "Lawrence East"
+            },
+            {
+              "id": 62,
+              "name": "Ellesmere"
+            },
+            {
+              "id": 63,
+              "name": "Midland"
+            },
+            {
+              "id": 64,
+              "name": "Scarborough Centre"
+            },
+            {
+              "id": 65,
+              "name": "McCowan"
+            }
+      
+      ]
+    },
+    {
+      "id": 4,
+      "brand_id": "Subway",
+      "short_name": "4",
+      "name": "Sheppard",
+      "branches": [
+            {
+              "id": 30,
+              "name": "Sheppard-Yonge"
+            },
+            {
+              "id": 66,
+              "name": "Bayview"
+            },
+            {
+              "id": 67,
+              "name": "Bessarion"
+            },
+            {
+              "id": 68,
+              "name": "Leslie"
+            },
+            {
+              "id": 69,
+              "name": "Don Mills"
+            }
+      
+      ]
+    }
+  ]
+}
