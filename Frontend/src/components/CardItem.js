a242f6cdeced14587d4dfecf7aa9262d8bd9cb4b
Yash Shah <mail@yashshah.com>
2016-08-18 21:33:50 +0530

    Handle the real data in CardItem

--- Frontend/src/components/CardItem.js
+++ Frontend/src/components/CardItem.js
@@ -4,6 +4,7 @@ import FlatButton from 'material-ui/FlatButton';
 import {StationView} from './StationView'
 import {Dialog} from './Dialog'
 import { Link } from 'react-router'
+import data from './data.js';
 
 const styles = {
   cardButtons: {
@@ -23,6 +24,7 @@ export class CardItem extends Component {
     }
     this.showDeleteDialog = this.showDeleteDialog.bind(this);
     this.closeDeleteDialog = this.closeDeleteDialog.bind(this)
+    this.getName = this.getName.bind(this);
   }
 
   showDeleteDialog(){
@@ -41,10 +43,28 @@ export class CardItem extends Component {
     this.props.onDelete(data);
   }
 
+  getName(obj, key, value){
+    let row = obj.filter(function( item ) {
+      return item[key] == value;
+    });
+    return row[0].name
+
+  }
+
   render() {
-    let title = `Disconnection between ${this.props.data.title}` 
-    let subtitle = this.props.data.subtitle
-    let link = <Link to={{ pathname: '/add', query: { line: 1, fromStation: 1, toStation: 5, description: "Testig" } }}/>
+    let line = this.getName(data.routes, "id", this.props.data.line);
+    let fromStation = this.getName(data.routes[this.props.data.line -1].branches, "id", this.props.data.fromStation);
+    let toStation = this.getName(data.routes[this.props.data.line -1].branches, "id", this.props.data.toStation);
+    let title = `Disconnection between ${fromStation} and ${toStation}` 
+    let subtitle = line
+    let query = {
+      line: this.props.data.line,
+      fromStation: this.props.data.fromStation,
+      toStation: this.props.data.toStation,
+      description: this.props.data.description,
+    }
+    let link = <Link to={{ pathname: '/add', query: query }}/>
+    let description = this.props.data.description
     return(
       <Card>
         <Dialog onDelete={this.handleDelete.bind(this, this.props.data)} onClose={this.closeDeleteDialog} open={this.state.deleteDialogOpen} />
@@ -60,6 +80,7 @@ export class CardItem extends Component {
           <FlatButton label="Delete" secondary={true} onTouchTap={this.showDeleteDialog}/>
         </CardActions>
         <CardText expandable={true}>
+          {description}
           <StationView />
         </CardText>
       </Card>
