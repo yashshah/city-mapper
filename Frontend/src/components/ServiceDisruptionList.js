b9bbc29f5846d28a58c418517d555f832d2a20a9
Yash Shah <mail@yashshah.com>
2016-08-20 14:27:22 +0530

    Fetching results directly from the server

--- Frontend/src/components/ServiceDisruptionList.js
+++ Frontend/src/components/ServiceDisruptionList.js
@@ -4,7 +4,8 @@ import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import {CardItem} from './CardItem';
-import serviceDisruptionData from './database.js'
+import axios from 'axios'
+import config from './config.js'
 import Snackbar from 'material-ui/Snackbar';
 
 const styles = {
@@ -23,12 +24,32 @@ export class ServiceDisruptionList extends Component {
     super(props);
     this.state = {
       deleteDialogOpen: false,
-      items: serviceDisruptionData.serviceDisruption
+      items: [],
       toastOpen: props.location.query.submit ? true : false,
       toastMessage: "Service disruption saved successfully!",
     }
     this.handleDelete = this.handleDelete.bind(this);
     this.handleRequestClose = this.handleRequestClose.bind(this);
+  }
+
+  componentDidMount() {
+    let self = this;
+    axios({
+      url: config.serverPath,
+      method: 'get',
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(config.username + ':' + config.password).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    .then(function (response) {
+      self.setState({
+        items: response.data 
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   handleRequestClose(){
@@ -40,13 +61,33 @@ export class ServiceDisruptionList extends Component {
   handleDelete(data){
     let updatedItems = this.state.items;
     updatedItems = updatedItems.filter(function( obj ) {
-        return obj.id !== data.id;
+      return obj.id !== data.id;
     });
-    this.setState({
-      deleteDialogOpen: false,
-      items: updatedItems
+    let self = this;
+    axios({
+      url: config.serverPath + data.id,
+      data: this.state,
+      method: 'delete',
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(config.username + ':' + config.password).toString('base64'))
+      },
+      json: true
+    })
+    .then(function (response) {
+      self.setState({
+        deleteDialogOpen: false,
+        items: updatedItems,
+        toastOpen: true,
+        toastMessage: "Service disruption deleted!"
+      });
+    })
+    .catch(function (error) {
+      self.setState({
+        deleteDialogOpen: false,
+        toastOpen: true,
+        toastMessage: "Error delete the service disruption!"
+      });
     });
-
   }
 
   render() {
@@ -64,7 +105,7 @@ export class ServiceDisruptionList extends Component {
           onRequestClose={this.handleRequestClose}
         />
       </div>
-    )
+      )
   }
 }
 
