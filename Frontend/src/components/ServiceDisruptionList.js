9c91993120014bcf2aa0487b07b06348b1e06dde
Yash Shah <mail@yashshah.com>
2016-08-20 14:23:33 +0530

    Remove unwanted data

--- Frontend/src/components/ServiceDisruptionList.js
+++ Frontend/src/components/ServiceDisruptionList.js
@@ -17,45 +17,6 @@ const styles = {
   },
 };
 
-// const serviceDisruptionData = [
-//   {
-//     title: 'Toronto - Paris',
-//     subtitle: '...Bond. James Bond.',
-//   },
-//   {
-//     title: 'New York - Boston',
-//     subtitle: 'Of all the gin joints in all the towns in all the world, she walks into mine.',
-//   },
-//   {
-//     title: 'San Fransico - Seattle',
-//     subtitle: 'Well, its not the men in your life that counts, its the life in your men.',
-//   },
-//   {
-//     title: 'Bangkok - Hanoi',
-//     subtitle: 'Would you be shocked if I put on something more comfortable?',
-//   },
-//   {
-//     title: 'Delhi - Bangalore',
-//     subtitle: 'My Mama always said, Life was like a box of chocolates; you never know what you are gonna get.',
-//   },
-//   {
-//     title: 'Rome - Venice',
-//     subtitle: 'I could dance with you till the cows come home...On second thought, I had rather dance with the cows when',
-//   },
-//   {
-//     title: 'Munich - Frankfurt',
-//     subtitle: 'Frankly, my dear, I dont give a damn!',
-//   },
-//   {
-//     title: 'Seattle - Portland',
-//     subtitle: 'You talking to me? You talking to me? You talking to me? Well, who the hell else are you talking to?',
-//   },
-//   {
-//     title: 'Barcelona - Madrid',
-//     subtitle: 'Gimme a whiskey, ginger ale on the side. And dont be stingy, baby.',
-//   },
-// ];
-
 export class ServiceDisruptionList extends Component {
   constructor(props) {
     super(props);
